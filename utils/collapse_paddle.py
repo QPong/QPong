bf11de6c7f6a0b2e2ed5bd8ea8078f6b71fdfb99
jare8734 <jare8734@colorado.edu>
2019-02-28 00:32:12 -0700

    collapse

--- utils/collapse_paddle.py
+++ utils/collapse_paddle.py
@@ -5,8 +5,8 @@ class Collapse(pygame.sprite.Sprite):
     def __init__(self, qubit_num):
         super().__init__()
 
-        self.image = pygame.Surface([20, 500/qubit_num])
-        self.image.fill((255,0,255, 1))
+        self.image = pygame.Surface([20, 500/2**qubit_num])
+        self.image.fill((255,0,255, 0))
 
         #self.image.blit()
 
@@ -17,17 +17,25 @@ class Collapse(pygame.sprite.Sprite):
         self.x = 1000
         self.y = 0
 
+        self.update()
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+
     def position(self, qubit_num, measurement):
         # self.image.fill(WHITE)
         self.image.fill((255,0,255, 1))
 
-        self.y = self.screenheight*measurement / qubit_num
+        self.y = self.screenheight*measurement / 2**qubit_num
+        self.update()
 
     def comp_position(self, qubit_num, measurement):
         self.image.fill(WHITE)
 
         self.x = 0
         self.y = self.screenheight*measurement / qubit_num
+        self.update()
 
     def reset(self):
         self.image.fill((WHITE, 0))
