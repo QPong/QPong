273013e22e64554f4a90c3ad487d4b805b79789d
Huang Junye <h.jun.ye@gmail.com>
2021-01-23 23:29:59 +0800

    fix new gate images color issue

--- controls/circuit_grid.py
+++ controls/circuit_grid.py
@@ -328,7 +328,7 @@ class CircuitGridGate(pygame.sprite.Sprite):
         node_type = self.circuit_grid_model.get_node_gate_part(self.wire_num, self.column_num)
 
         if node_type == node_types.H:
-            self.image, self.rect = load_image('gate_images/h_gate.png', -1)
+            self.image, self.rect = load_image('gate_images/h_gate.png')
         elif node_type == node_types.X:
             node = self.circuit_grid_model.get_node(self.wire_num, self.column_num)
             if node.ctrl_a >= 0 or node.ctrl_b >= 0:
@@ -339,38 +339,38 @@ class CircuitGridGate(pygame.sprite.Sprite):
                 else:
                     self.image, self.rect = load_image('gate_images/not_gate_above_ctrl.png', -1)
             elif node.radians != 0:
-                self.image, self.rect = load_image('gate_images/rx_gate.png', -1)
+                self.image, self.rect = load_image('gate_images/rx_gate.png')
                 self.rect = self.image.get_rect()
                 pygame.draw.arc(self.image, MAGENTA, self.rect, 0, node.radians % (2 * np.pi), 6)
                 pygame.draw.arc(self.image, MAGENTA, self.rect, node.radians % (2 * np.pi), 2 * np.pi, 1)
             else:
-                self.image, self.rect = load_image('gate_images/x_gate.png', -1)
+                self.image, self.rect = load_image('gate_images/x_gate.png')
         elif node_type == node_types.Y:
             node = self.circuit_grid_model.get_node(self.wire_num, self.column_num)
             if node.radians != 0:
-                self.image, self.rect = load_image('gate_images/ry_gate.png', -1)
+                self.image, self.rect = load_image('gate_images/ry_gate.png')
                 self.rect = self.image.get_rect()
                 pygame.draw.arc(self.image, MAGENTA, self.rect, 0, node.radians % (2 * np.pi), 6)
                 pygame.draw.arc(self.image, MAGENTA, self.rect, node.radians % (2 * np.pi), 2 * np.pi, 1)
             else:
-                self.image, self.rect = load_image('gate_images/y_gate.png', -1)
+                self.image, self.rect = load_image('gate_images/y_gate.png')
         elif node_type == node_types.Z:
             node = self.circuit_grid_model.get_node(self.wire_num, self.column_num)
             if node.radians != 0:
-                self.image, self.rect = load_image('gate_images/rz_gate.png', -1)
+                self.image, self.rect = load_image('gate_images/rz_gate.png')
                 self.rect = self.image.get_rect()
                 pygame.draw.arc(self.image, MAGENTA, self.rect, 0, node.radians % (2 * np.pi), 6)
                 pygame.draw.arc(self.image, MAGENTA, self.rect, node.radians % (2 * np.pi), 2 * np.pi, 1)
             else:
-                self.image, self.rect = load_image('gate_images/z_gate.png', -1)
+                self.image, self.rect = load_image('gate_images/z_gate.png')
         elif node_type == node_types.S:
-            self.image, self.rect = load_image('gate_images/s_gate.png', -1)
+            self.image, self.rect = load_image('gate_images/s_gate.png')
         elif node_type == node_types.SDG:
-            self.image, self.rect = load_image('gate_images/sdg_gate.png', -1)
+            self.image, self.rect = load_image('gate_images/sdg_gate.png')
         elif node_type == node_types.T:
-            self.image, self.rect = load_image('gate_images/t_gate.png', -1)
+            self.image, self.rect = load_image('gate_images/t_gate.png')
         elif node_type == node_types.TDG:
-            self.image, self.rect = load_image('gate_images/tdg_gate.png', -1)
+            self.image, self.rect = load_image('gate_images/tdg_gate.png')
         elif node_type == node_types.IDEN:
             # a completely transparent PNG is used to place at the end of the circuit to prevent crash
             # the game crashes if the circuit is empty
