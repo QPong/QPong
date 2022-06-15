ca4e8bc26050ab430d69b142247a2f53dc65d469
Unathi Skosana <ukskosana@gmail.com>
2022-06-16 00:33:19 +0200

    chore: organize repository with ecosystem template

---
+++ qpong/utils/score.py
@@ -0,0 +1,32 @@
+"""
+TODO
+"""
+import pygame
+
+class Score(pygame.sprite.Sprite):
+    """
+    TODO
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.player = 0
+        self.computer = 0
+
+    # Player = 0, Computer = 1
+    def update(self, score):
+        if score == 0:
+            self.computer += 1
+
+        if score == 1:
+            self.player += 1
+
+    def get_score(self, player):
+        if player == 0:
+            return self.computer
+        if player == 1:
+            return self.player
+
+    def reset_score(self):
+        self.computer = 0
+        self.player = 0
