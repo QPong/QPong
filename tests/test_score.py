99e817368ac45ab4b9d2e71a33ad93a7f0228543
Unathi Skosana <ukskosana@gmail.com>
2022-07-06 20:04:01 +0200

    chore: add unittests for score

---
+++ tests/test_score.py
@@ -0,0 +1,82 @@
+#
+# Copyright 2019 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Test score
+"""
+
+import unittest
+
+from qpong.utils.score import Score
+
+
+class TestScene(unittest.TestCase):
+    """
+    Unit tests for score
+    """
+
+    def test_score_initialization(self):
+        """
+        Test score initialization
+        """
+
+        score = Score()
+
+        self.assertEqual(score.player, 0)
+        self.assertEqual(score.computer, 0)
+
+    def test_score_update(self):
+        """
+        Test score update
+        """
+        score = Score()
+
+        score.update(0)
+
+        self.assertEqual(score.computer, 1)
+        self.assertEqual(score.player, 0)
+
+        score.update(1)
+
+        self.assertEqual(score.computer, 1)
+        self.assertEqual(score.player, 1)
+
+    def test_score_reset(self):
+        """
+        Test score reset
+        """
+        score = Score()
+
+        score.update(0)
+        score.update(1)
+
+        score.reset_score()
+
+        self.assertEqual(score.computer, 0)
+        self.assertEqual(score.player, 0)
+
+    def test_score_getter(self):
+        """
+        Test score getter
+        """
+
+        score = Score()
+
+        score.update(0)
+        score.update(1)
+
+        self.assertEqual(score.get_score(0), 1)
+        self.assertEqual(score.get_score(1), 1)
